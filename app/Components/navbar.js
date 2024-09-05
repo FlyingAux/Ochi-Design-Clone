8ccf368a04bb25731af4426460898ed7f2ff80eb
rajdev04 <rajdevshrivastava72@gmail.com>
2024-09-05 13:16:47 +0530

    Landing

---
+++ app/Components/navbar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Link from 'next/link'
+
+const navbar = () => {
+  return (
+    <>
+        <div className='Navbar_cont h-20 w-full font-gilroy flex items-center justify-between px-8 fixed z-[999]'>
+            <div className='logo'>
+                <h1 className="text-4xl font-semibold">Ochi</h1>
+            </div>
+            <div className='links flex gap-10'>
+                {['Services','Our work','About us','Insights','Contact us'].map((item,index)=>(
+                    <Link href='' key={index} className={`capitalize text-lg font-normal tracking-wide ${index === 4 && "ml-32" }`}>{item}</Link> 
+                ))}
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default navbar
--- app/page.js
+++ app/page.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './Components/navbar'
+import Landing from './Components/Landing'
 
 const page = () => {
   return (
-    <div className=''>page</div>
+    <>
+    <Navbar/>
+    <Landing/>
+    </>
   )
 }
 
