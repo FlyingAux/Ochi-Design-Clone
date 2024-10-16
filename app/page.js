4a6c9321f5ef875f7567922840eae1674b39be2a
rajdev04 <rajdevshrivastava72@gmail.com>
2024-10-16 07:21:29 +0530

    o

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Components/navbar'
 import Landing from './Components/Landing'
+import Marquee from './Components/marquee'
 
 const page = () => {
   return (
     <>
     <Navbar/>
     <Landing/>
+    <Marquee/>
     </>
   )
 }
